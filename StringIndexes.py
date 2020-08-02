22d50353ede0fdc5352ff213bd8cb68b8385ddeb
Raghu Mahajan <raghu7mahajan@hotmail.com>
2020-08-02 18:05:27 +0530

    StringIndexes

---
+++ StringIndexes.py
@@ -0,0 +1,17 @@
+#         0123456789
+parrot = "Norwegian Blue"
+
+print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+print(parrot[-11])
+print(parrot[-10])
+print()
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
