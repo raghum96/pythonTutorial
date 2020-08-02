0a8d8928a5ed441d23357efeb764059af8850861
Raghu Mahajan <raghu7mahajan@hotmail.com>
2020-08-02 16:52:50 +0530

    Strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+
+print("hello" + "world")
+
+name = "Raghu"
+print(name)
+
+enterName = input("Please enter name")
+print(enterName)
