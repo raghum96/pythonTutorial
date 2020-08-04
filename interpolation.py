99e0285c3fd518b4b09cee395dcf5821749137b9
Raghu Mahajan <raghu7mahajan@hotmail.com>
2020-08-04 21:17:48 +0530

    Create interpolation.py

---
+++ interpolation.py
@@ -0,0 +1,9 @@
+age = 24
+print("My age is %d" % age)
+
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s" % (age, major, 6, minor))
+
+print("pi = %f" % (22 / 7))
+print("pi = %60.50f" % (22 / 7))
